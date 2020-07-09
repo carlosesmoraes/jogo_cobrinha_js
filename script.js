cc05b806855d8a8f6bd874fe5f230bed28437b0b
carlosesmoraes <66447511+carlosesmoraes@users.noreply.github.com>
2020-07-09 19:15:13 -0300

    criar cobrinha

--- script.js
+++ script.js
@@ -1,9 +1,21 @@
 let canvas = document.getElementById("snake");
 let context = canvas.getContext("2d");
 let Box = 32;
+let snake = [];
+snake[0] = {
+    x: 8 * Box,
+    y: 8 * Box
+}
 
 function criarBG(){
-    context.fillStyle = "lightscreen";
-    context.fillRect(0, 0, 16 * Box, 16 * Box)
+    context.fillStyle = "lightgreen";
+    context.fillRect(0, 0, 16 * Box, 16 * Box);
 }
-criarBG()
+function criarCobrinha(){
+    for(i=0; i < snake.length; i++){
+        context.fillStyle="green"
+        context.fillRect(snake[i].x, snake[i].y, Box, Box);
+    }
+}
+criarBG();
+criarCobrinha();
